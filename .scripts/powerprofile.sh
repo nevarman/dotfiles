716d3eb63877dcda3c6a723b32dd08b92aad8cd3
Nevzat Arman <nevzatarman@gmail.com>
2020-02-10 20:07:37 +0100

    added system76 power rofi

---
+++ .scripts/powerprofile.sh
@@ -0,0 +1,20 @@
+pro=$(system76-power profile)
+
+battery="battery"
+balanced="balanced"
+performance="performance"
+# check current
+if [[ $pro  == *"Battery"* ]]; then
+   battery="${battery} (Current)"
+fi
+if [[ $pro  == *"Balanced"* ]]; then
+   balanced="${balanced} (Current)"
+fi
+if [[ $pro  == *"Performance"* ]]; then
+   performance="${performance} (Current)"
+fi
+# select and apply
+selection="${battery}\n ${balanced}\n ${performance}"
+profile=$(echo "$selection" | rofi -dmenu)
+newprofile=$(system76-power profile $profile)
+notify-send "$newprofile" -u low
